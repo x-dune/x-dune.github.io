3391b8f40374988b0356b3f7a5e69ffee35026e4
adamdune <adam.ho.buss@gmail.com>
2020-09-30 23:59:28 +0800

    setting up for ghpages deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
     extend: {
       backgroundImage: () => ({
-        banner: "url('/pexels-photo-2058911.334083fe1d2b825ddb72.jpeg')",
+        banner: "url('/images/pexels-photo-2058911.334083fe1d2b825ddb72.jpeg')",
       }),
       colors: () => ({
         "transparent-purple-1": "rgb(58, 10, 156, 0.6)",
