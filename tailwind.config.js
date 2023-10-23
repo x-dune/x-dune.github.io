a430497ad4194088d20f38c0b5a36b15d8aae006
x-dune <39018755+x-dune@users.noreply.github.com>
2023-10-23 20:57:26 +0800

    chore: fix broken stuff
    - updated package-lock.json to modern schema
    - fixed GithubIcon.js due to case sensitivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: () => ({
-        banner: "url('/images/pexels-photo-2058911.334083fe1d2b825ddb72.jpeg')",
+        banner: "url('../public/images/pexels-photo-2058911.334083fe1d2b825ddb72.jpeg')",
       }),
       colors: () => ({
         "transparent-purple-1": "rgb(58, 10, 156, 0.6)",
